b287a7082f9da5f1bae16154767ee23f7485cb48
lerishka <tswetochlerch@gmail.com>
2025-08-13 23:18:57 +0300

    Update project changes

--- src/components/CardList/CardList.test.tsx
+++ src/components/CardList/CardList.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
 import CardList from "./CardList";
 
